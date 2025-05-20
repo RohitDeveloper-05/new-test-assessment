70fa3a7a57f418b93acf69eea04616b3fd168c26
RohitDeveloper-05 <rohitsbelgaum@gmail.com>
2025-05-20 08:39:58 +0530

    Successfully Inserted into the Database

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 //Trying the Supabase thing. Let's see if it works or not 
 //I have changed the field from scores to score in the database table
+//Successfully Inserted into the Database
 
 const middlewares = require('./middlewares');
 const api = require('./api');
